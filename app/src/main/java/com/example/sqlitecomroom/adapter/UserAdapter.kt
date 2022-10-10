5eb9baec9df73e62dd57071b30e8a0c8bded5fc9
Hickdpaula <hickdpaula@gmail.com>
2022-10-10 09:05:23 -0300

    SQLite com Room

---
+++ app/src/main/java/com/example/sqlitecomroom/adapter/UserAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.sqlitecomroom.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sqlitecomroom.data.Usuario
+import com.example.sqlitecomroom.databinding.CardLayoutBinding
+
+class UserAdapter : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    private var listUser = emptyList<Usuario>()
+
+    class UserViewHolder(val binding: CardLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        return UserViewHolder(CardLayoutBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = listUser[position]
+
+        holder.binding.txtId.text = user.id.toString()
+        holder.binding.txtNome.text = user.nome
+        holder.binding.txtSobrenome.text = user.sobrenome
+        holder.binding.txtIdade.text = user.idade.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return listUser.size
+    }
+
+    fun setList(list: List<Usuario>){
+        listUser = list
+        notifyDataSetChanged()
+    }
+
+
+}
